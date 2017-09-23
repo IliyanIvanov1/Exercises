d81f38ec56b5ff6aa8968cf4a905299e844ccce7
Iliyan N. Ivanov <ilian4o1111@abv.bg>
2017-09-23 18:23:07 +0300

    Adding project

---
+++ LambdaLINQ/DefaultValue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            var input = Console.ReadLine();
+            while (input != "end")
+            {
+                var inputParams = input.Split(new[] { " ", "-", ">" }, StringSplitOptions.RemoveEmptyEntries);
+
+                var key = inputParams[0];
+                var value = inputParams[1];
+
+                dictionary[key] = value;
+
+                    
+                input = Console.ReadLine();
+            }
+            var defaultValue = Console.ReadLine();
+
+            var unreplacedValues = dictionary.Where(x => x.Value != "null").OrderByDescending(x => x.Value.Length).ToDictionary(x=>x.Key,x=>x.Value);
+
+            foreach (var kvp in unreplacedValues)
+            {
+                Console.WriteLine($"{kvp.Key} <-> {kvp.Value}");
+            }
+
+            var replacedValues = dictionary.Where(x => x.Value == "null").ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var kvp in replacedValues)
+            {
+                Console.WriteLine($"{kvp.Key} <-> {defaultValue}");
+            }
+        }
+    }
+}
---
+++ LambdaLINQ/RegisteredUsers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace RegisteredUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var registeredUsers = new Dictionary<string, DateTime>();
+
+            var input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                var inputParams = input.Split(new[] { " ","-",">" }, StringSplitOptions.RemoveEmptyEntries);
+
+                var username = inputParams[0];
+                var registryDate = DateTime.ParseExact(inputParams[1],"dd/MM/yyyy",CultureInfo.InvariantCulture);
+
+                registeredUsers[username] = registryDate;
+
+                input = Console.ReadLine();
+            }
+            var orderedUsernames = registeredUsers
+                .Reverse()
+                .OrderBy(x=>x.Value)
+                .Take(5)
+                .OrderByDescending(x=>x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine(string.Join(Environment.NewLine, orderedUsernames.Keys));
+        }
+    }
+}
