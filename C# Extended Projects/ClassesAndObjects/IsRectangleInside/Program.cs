2984472cd3751736c19614c993a08c819e8f1e70
Iliyan N. Ivanov <ilian4o1111@abv.bg>
2017-09-25 00:35:18 +0300

    Commit

---
+++ C# Extended Projects/ClassesAndObjects/IsRectangleInside/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsRectangleInside
+{
+    public class Rectangle
+    {
+        public int Top { get; set; }
+
+        public int Left { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int Bottom
+        {
+            get
+            {
+                return Top + Height;
+            }
+        }
+        
+        public int Right
+        {
+            get
+            {
+                return Left + Width;
+            }
+        }
+
+        public bool IsInside(Rectangle rectangle)
+        {
+            var leftIsValid = rectangle.Left <= Left;
+            var topIsValid = rectangle.Top <= Top;
+            var rightIsValid = rectangle.Right >= Right;
+            var bottomIsValid = rectangle.Bottom >= Bottom;
+
+            return leftIsValid && topIsValid && rightIsValid && bottomIsValid;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstRectangle = CreateRectangle();
+            var secondRectangle = CreateRectangle();
+
+            var result = firstRectangle.IsInside(secondRectangle);
+
+            var printResult = result ? "Inside" : "Not inside";
+
+            Console.WriteLine(printResult);
+        }
+        public static Rectangle CreateRectangle()
+        {
+            var rectangleParts = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            return new Rectangle
+            {
+                Left = rectangleParts[0],
+                Top = rectangleParts[1],
+                Width = rectangleParts[2],
+                Height = rectangleParts[3]
+            };
+        }
+
+    }
+}
