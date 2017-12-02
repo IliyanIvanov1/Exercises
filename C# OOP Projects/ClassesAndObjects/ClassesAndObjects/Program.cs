3dddcd0936eaa5b90f01064a735b2a6aec411975
Iliyan N. Ivanov <ilian4o1111@abv.bg>
2017-12-02 20:41:44 +0200

    Commit

---
+++ C# OOP Projects/ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        public class Point
+        {
+            public double X { get; set; }
+
+            public double Y { get; set; }
+
+
+        }
+        static void Main(string[] args)
+        {
+            var firstPoint = CreatePoint();
+            var secondPoint = CreatePoint();
+
+            var result = Distance(firstPoint, secondPoint);
+
+            Console.WriteLine($"{result:F3}");
+        }
+        
+        public static Point CreatePoint()
+        {
+            var point = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+            return new Point
+            {
+                X = point[0],
+                Y = point[1]
+            };
+
+        }
+        public static double Distance(Point firstPoint, Point secondPoint)
+        {
+            var squareX = Math.Pow(firstPoint.X - secondPoint.X, 2);
+            var sqaureY = Math.Pow(firstPoint.Y - secondPoint.Y, 2);
+
+            var result = Math.Sqrt(squareX + sqaureY);
+
+            return result;
+        }
+    }
+
+}
---
+++ C# OOP Projects/ClassesAndObjects/Sales/Sales.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales
+{
+    class Sales
+    {
+        public string Town { get; set; }
+
+        public string Product { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public static Sales CreateSale(string saleAsString)
+        {
+            var saleParts = saleAsString.Split(' ');
+
+            return new Sales
+            {
+                Town = saleParts[0],
+                Product = saleParts[1],
+                Price=decimal.Parse(saleParts[2]),
+                Quantity=decimal.Parse(saleParts[3])
+
+            };
+
+        }
+    }
+}
