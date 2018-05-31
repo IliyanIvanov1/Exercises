22f7cc956dfc114d2f49d08115bc7c6cd6bae19d
Iliyan N. Ivanov <ilian4o1111@abv.bg>
2018-06-01 00:00:49 +0300

    Change

--- C# Extended Projects/ArraySorting/ArraySorting/Program.cs
+++ C# Extended Projects/ArraySorting/ArraySorting/Program.cs
@@ -1,38 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArraySorting
+﻿namespace ArraySorting
 {
+    using System;
     public class Program
     {
         public static void Main()
         {
-            //int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+           //int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
-            int[] array = new int[1000000];
-
+            int[] array = new int[100000];
+            
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = array.Length - i;
             }
 
-            for (int i = 0; i < array.Length - i; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 var pos = 0;
-                var currentElement = array[i];
-                if (currentElement > array[pos])
+                for (int j = 0; j < array.Length-i; j++)
                 {
-                    pos = i;
+                    var currentElement = array[j];
+                    if (currentElement > array[pos])
+                    {
+                        pos = j;
+                    }
                 }
                 var temp = array[pos];
                 array[pos] = array[array.Length - 1 - i];
                 array[array.Length - 1 - i] = temp;
             }
 
-            Console.WriteLine(string.Join(Environment.NewLine, array));
+            Console.WriteLine(string.Join(Environment.NewLine,array));
         }
     }
 }
